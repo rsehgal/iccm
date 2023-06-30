8a4f987aed2e3582e05c1fbda72e85cdf8d9c345
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-06-30 11:00:02 +0530

    Added PDF for steps for registration and abstract submission

--- controller.php
+++ controller.php
@@ -227,10 +227,10 @@ function Home(){
     </tr>
     
      <tr class="text-center">
-     <td><a href="#"  class="blink-soft font-weight-bold" >Steps for ICCM registration </a></td>
+     <td><a href="PDF/Steps_for_registration.pdf"  class="blink-soft font-weight-bold" >Steps for ICCM registration </a></td>
      </tr> 
      <tr class="text-center">
-     <td><a href="#" class="blink-hard font-weight-bold" >Steps for Abstract submission </a></td>
+     <td><a href="PDF/Steps_for_ Abstract_Submission.pdf" class="blink-hard font-weight-bold" >Steps for Abstract submission </a></td>
      </tr> 
 
     <tr style="font-size:16px; color:#FFF;">
