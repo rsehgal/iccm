d4bc654b4dad419b2799a148d8596ac0fd3b2331
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-08-31 09:23:00 +0530

    Changed the submission end date to 07 Sep 2023, as requested by Jyoti prakash

--- controller.php
+++ controller.php
@@ -202,7 +202,7 @@ function Home(){
     
     
     <h1 class="text-red blink-soft">
-    <span style="color:#900;"><strong> Deadline of abstract (extended) submission is August 30, 2023 </strong></span> 
+    <span style="color:#900;"><strong> Deadline of abstract (extended) submission is September 07, 2023 </strong></span> 
     </h1>
     <br>
     
