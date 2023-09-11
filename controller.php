aa7e4edfebf4fbbafcd9e1f7662c22f339e7ae49
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-09-11 12:19:49 +0530

    Change the abstract submission message on the main page as requested by Jyoti prakash

--- controller.php
+++ controller.php
@@ -202,7 +202,8 @@ function Home(){
     
     
     <h1 class="text-red blink-soft">
-    <span style="color:#900;"><strong> Deadline of abstract (extended) submission is September 07, 2023 </strong></span> 
+    <!-- <span style="color:#900;"><strong> Deadline of abstract (extended) submission is September 07, 2023 </strong></span>  -->
+    <span style="color:#900;"><strong> Last date of the abstract submission is over. </strong></span> 
     </h1>
     <br>
     
